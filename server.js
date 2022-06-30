e21a9023263b48c1ba57f1f6ad5fda2838c0e005
truncgil <github@truncgil.com>
2022-06-30 17:23:18 +0300

    server client id definition system has been changed

--- server.js
+++ server.js
@@ -6,7 +6,16 @@ var clients = [];
 var client_ips = [];
 var client_ids = [];
 
-
+function ascii_to_hex(str)
+  {
+	var arr1 = [];
+	for (var n = 0, l = str.length; n < l; n ++) 
+     {
+		var hex = Number(str.charCodeAt(n)).toString(16);
+		arr1.push(hex);
+	 }
+	return arr1.join(' ');
+   }
 // Start a TCP Server
 net.createServer(function(socket) {
   console.log(socket.remoteAddress);  
@@ -19,10 +28,10 @@ net.createServer(function(socket) {
   clients.push(socket);
   console.log(client_ips);
   let count = Object.keys(client_ips).length;
-console.log(socket.name);
+  console.log(socket.name);
   // Send a nice welcome message and announce
-  socket.write("Welcome " + socket.name + "\n\r");
-  console.log(count + " cihaz bağlı");
+  socket.write("Olimpiyat - Truncgil IOT TCP/Server v1.0");
+  //console.log(count + " cihaz bağlı");
 
 
 
@@ -32,11 +41,18 @@ console.log(socket.name);
   var message = '';
   // Handle incoming messages from clients.
   socket.on('data', function(data) {
-    client_ids[data] = socket;
-  //  console.log(data);
+
     // copy incoming data to message
     message += data
-    var n = message.indexOf('\n')
+    var n = message.indexOf('\n');
+    if(message.length==12) {
+        client_ids[message] = socket;
+        console.log("Client ID Login: " + message);
+    } else {
+        console.log("client send: " + message + "\n");
+    }
+    
+   
     // if we have a \n? in message then emit one or more 'line' events
     var is_private = message.indexOf('::');
     //console.log(is_private);
@@ -44,25 +60,45 @@ console.log(socket.name);
         let split = message.split('::');
         let client_id = split[0];
         let private_message = split[1].trim(); 
-        console.log("client_id:" + client_id);
-        console.log("private_message:" + private_message);
+        let sender = socket;
+        console.log("client_id: " + client_id);
+        console.log("register_code: " + private_message);
         try {
-            client_ids[client_id].write(private_message);
+            let private_message_hex = ascii_to_hex(private_message);
+            console.log(private_message_hex);
+            let sonuc = client_ids[client_id].write(private_message);
+            console.log("send: " + client_id);
+            var yanit = '';
+            client_ids[client_id].on('data',function(data){
+                console.log("reply: ");
+                yanit = data;
+                console.log(yanit);
+                sender.write(yanit);
+            });
+            client_ids[client_id].on('error',function(err){
+              console.log(err);
+              
+            });
         } catch (error) {
-            console.error("cihaz online değil veya yanlış id gönderilmiştir.");
+            var error_message = "\nSend Error: " + client_id + '\n';
+            console.error(error_message);
             console.error(error);
+            sender.write(error_message);
         }
         
     }
-    socket.emit('line', message.substring(0, n))
+    /*
+     socket.emit('line', message.substring(0, n))
       message = message.substring(n + 1)
       n = message.indexOf('\n')
-      console.log(n);
-      /*
+     // console.log(n);
+    
+      
     while (~n) {
       socket.emit('line', message.substring(0, n))
       message = message.substring(n + 1)
       n = message.indexOf('\n')
+      console.log("line");
       console.log(n);
     }
     */
@@ -70,14 +106,14 @@ console.log(socket.name);
 
   // Broadcast on end of line
   socket.on('line', function() {
-    broadcast(socket.name + "> " + message, socket);
+   // broadcast(socket.name + "> " + message, socket);
     message = '';
   })
 
   // Remove the client from the list when it leaves
   socket.on('end', function() {
     clients.splice(clients.indexOf(socket), 1);
-    broadcast("\n\r" + socket.name + " left the chat." + "\n\r");
+   // broadcast("\n\r" + socket.name + " left the chat." + "\n\r");
   });
 
   // Send a message to all clients
